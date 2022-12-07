a1ebe5db90e095baeaca5fabf43d500028d9d7a4
Thomas Aubin <thomascontact12@gmail.com>
2022-12-07 09:42:23 -0500

    Added welcome page and IO functions

--- 2.5-Implementation/controller.py
+++ 2.5-Implementation/controller.py
@@ -2,3 +2,14 @@ import view
 
 def launch():
     view.showSplashPage()
+
+    choice = view.welcomePage()
+    if (choice == 1):
+        pass
+        # Register
+    elif (choice == 2):
+        pass
+        # Login
+    else:
+        view.closePage()
+        quit()
--- 2.5-Implementation/view.py
+++ 2.5-Implementation/view.py
@@ -8,3 +8,45 @@ def showSplashPage():
     print("     Online Bookstore\n")
     print("    \"Knowledge is power!\"")
     print("=============================")
+
+def welcomePage():
+    print("\n\nHow can we help you today?")
+    print("1. Register")
+    print("2. Login")
+    print("3. Quit")
+
+    return getProperInput(3)
+
+def closePage():
+    print("\n\nGoodbye. Come again!")
+
+
+
+
+# ==================
+# Helper Functions
+# ==================
+
+def getProperInput(numOptions):
+    while True:
+        choice = input("\n>> ")
+
+        choice = convertStringToInt(choice)
+        if type(choice) == int:
+            if validateNumberChoice(numOptions, choice):
+                return choice
+        
+        print("Bad input. Please enter again")
+
+def convertStringToInt(n):
+    try:
+        n = int(n)
+        return n
+    except:
+        return False
+
+def validateNumberChoice(numOptions, choice):
+    if choice in range(1, numOptions+1):
+        return True
+    else:
+        return False
