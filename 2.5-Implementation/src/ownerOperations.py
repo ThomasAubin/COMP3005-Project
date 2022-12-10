71fb60ee2313766675d7e2b6f73aac55a9a621df
ScaryPotatoz <jasonhuynh.321@gmail.com>
2022-12-10 11:46:36 -0500

    local test on register and owneroperations done, it works

---
+++ 2.5-Implementation/src/ownerOperations.py
@@ -0,0 +1,77 @@
+import psycopg2
+
+# set your database details here
+hostname = 'localhost'
+database = 'Look_Inna_Book'
+username = 'postgres'
+pwd = 'Neverletitrun!5'
+portId = '5432'
+
+
+#connect to data base
+connection = psycopg2.connect(
+    host = hostname,
+    dbname = database,
+    user = username,
+    password = pwd,
+    port = portId)
+
+#create a cursor for querying
+cursor = connection.cursor()
+
+
+def addBook():
+    #get the book data
+    print("Lets start adding a book!")
+    isbn = input("What is the isbn of the book: ")
+    name = input("What is the title of the book: ")
+    pages = input("How many pages are in the book: ")
+    price = input("What is the price of the book: ")
+    quantity = input("How many of these books do you have in stock: ")
+    percent = input("What is the publisher percentage: ")
+    email = input("What is the publisher's email: ")
+
+    #add the book
+    print("Adding book...")
+    cursor.execute("INSERT INTO Books(ISBN, name, pages, price, quantity, publisherSharePercentage, publisherEmail) VALUES (%s, %s, %s, %s, %s, %s, %s)", (isbn, name, pages, float(price), int(quantity), int(percent), email))
+
+    #make sure it added
+    cursor.execute("SELECT * FROM Books WHERE ISBN = %s", (isbn,))
+    if cursor.fetchone() is not None:
+        print("Add successful")
+
+
+
+def removeBook():
+    #get book isbn
+    isbn = input("What is the ISBN for the book you want to delete? ")
+    print("Okay, deleting now...")
+
+    #make sure book exists
+    cursor.execute("SELECT * FROM Books WHERE ISBN = %s", (isbn,))
+
+    if cursor.fetchone() is not None:
+        #delete book
+        cursor.execute("DELETE FROM Books WHERE ISBN = %s", (isbn,))
+        print("Deletion complete")
+    
+    #if book doesnt exist
+    else:
+        print("Sorry, a book with that ISBN does not exist")
+        while True:
+            userIn = input("Would you like to delete something else? (y/n)")
+            if userIn == "y" or userIn == "Y":
+                removeBook()
+                return
+            elif userIn == "n" or userIn == "N":
+                return
+            else:
+                print("Sorry that was an invalid input, please type 'y' for yes or 'n' for no")
+
+
+#save data
+connection.commit()
+
+#close connections and cursor
+cursor.close()
+connection.close()
--- 2.5-Implementation/src/register.py
+++ 2.5-Implementation/src/register.py
@@ -5,89 +5,79 @@ def register():
     hostname = 'localhost'
     database = 'Look_Inna_Book'
     username = 'postgres'
-    pwd = 'password'
+    pwd = 'Neverletitrun!5'
     portId = '5432'
 
     #initialise the connection and cursor
     connection = None
     cursor = None
 
-    try:
-        #connect to data base
-        connection = psycopg2.connect(
-            host = hostname,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = portId)
-        
-        #create a cursor for querying
-        cursor = connection.cursor()
+#    try:
+    #connect to data base
+    connection = psycopg2.connect(
+        host = hostname,
+        dbname = database,
+        user = username,
+        password = pwd,
+        port = portId)
+    
+    #create a cursor for querying
+    cursor = connection.cursor()
 
 
 
-        #asks for the users information and stores data
-        print("\n\n================User Info==============\n\n")
-        fname = input("First Name: ")
-        lname = input("Last Name: ")
+    #asks for the users information and stores data
+    print("\n\n================User Info==============\n\n")
+    fname = input("First Name: ")
+    lname = input("Last Name: ")
 
-        print("\n\n================Address Info==============\n\n")
-        streetNum = input("Street Number: ")
-        street = input("Street Name: ")
-        city = input("City: ")
-        postalCode = input("Postal Code: ")
-        country = input("Country: ")
-        type = "home" #put home as default
-        name = input("Name of the address: ")
-
-
-
-        #create the user table if not exist
-        createUser = ''' CREATE TABLE IF NOT EXISTS Users (
-            UID     SERIAL PRIMARY KEY,
-            fname   varchar(100) NOT NULL,
-            lname   varchar(100) NOT NULL)    '''
-
-        #create the address table if not exist
-        createAddr = ''' CREATE TABLE IF NOT EXISTS Address (
-            UID         SERIAL PRIMARY KEY,
-            streetNum   int NOT NULL,
-            street      varchar(100) NOT NULL,
-            city        varchar(100) NOT NULL,
-            postalCode  varchar(100) NOT NULL,
-            country     varchar(100) NOT NULL,
-            type        varchar(100) NOT NULL,
-            name        varchar(100) NOT NULL)    '''
+    print("\n\n================Address Info==============\n\n")
+    streetNum = input("Street Number: ")
+    street = input("Street Name: ")
+    city = input("City: ")
+    postalCode = input("Postal Code: ")
+    country = input("Country: ")
+    type = "home" #put home as default
+    name = input("Name of the address: ")
 
 
+    #adding the user to the table
+    insertUser = 'INSERT INTO Users (UID, fname, lname) VALUES (%s, %s, %s)'
+    userValue = (2, fname, lname) #dont know what to put for UID
 
-        #adding the user to the table
-        insertUser = 'INSERT INTO Users (UID, fname, lname) VALUES (%s, %s, %s)'
-        userValue = (1, fname, lname) #dont know what to put for UID
+    #adding the address to the table
+    insertAddr = 'INSERT INTO Address (UID, streetNum, street, city, postalCode, country, type, name) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+    addrValue = (2, streetNum, street, city, postalCode, country, type, name) #dont know what to put for UID
 
-        #adding the address to the table
-        insertAddr = 'INSERT INTO Address (UID, streetNum, street, city, postalCode, country, type, name'
-        addrValue = (1, streetNum, street, city, postalCode, country, type, name) #dont know what to put for UID
+    #exectute commands
+    cursor.execute(insertUser, userValue)
+    cursor.execute(insertAddr, addrValue)
 
+    #check to make sure added MUST CHANGE TO UID******
+    cursor.execute("SELECT * FROM users WHERE fname = %s", (fname,))
+    addUser = cursor.fetchone()
 
-        #exectute commands
-        cursor.execute(createUser)
-        cursor.execute(createAddr)
-        cursor.execute(insertUser, userValue)
-        cursor.execute(insertAddr, addrValue)
+    #check to make sure added MUST CHANGE TO UID******
+    cursor.execute("SELECT * FROM address WHERE street = %s", (street,))
+    addAddress = cursor.fetchone()
 
-        #save the new things to the database
-        connection.commit()
+    if addUser is not None and addAddress is not None:
+        print("Registration successful")
 
-        #close connections and cursor
-        cursor.close()
-        connection.close()
+    #save the new things to the database
+    connection.commit()
 
-    #error handling
-    except Exception as error:
-        print(error)
-    finally:
-        if cursor is not None:
-            cursor.close()
-        if connection is not None:
-            connection.close()
+    #close connections and cursor
+    cursor.close()
+    connection.close()
+
+    # #error handling
+    # except Exception as error:
+    #     print(error)
+    # finally:
+    #     if cursor is not None:
+    #         cursor.close()
+    #     if connection is not None:
+    #         connection.close()
+
+register()
